360ff65b748f3b5118badcef2fd6edb6520e04ac
Ricki Bin Yamin <ricki.bin.yamin@gmail.com>
2025-04-23 06:38:06 +0700

    Initial Commit

---
+++ HelloDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloDemo
+//
+//  Created by Ricki Bin Yamin on 23/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
